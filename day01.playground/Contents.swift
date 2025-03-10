a908628bb79b81836aa59c479f0f532e598db472
JC Dela Cuesta <johndelacuesta@gmail.com>
2025-03-10 12:11:49 -0700

    Uploaded Playground File for Day 1/100
    **Concepts Covered:**  
    - Intro to Swift
    - Variables and constants
    - Strings
    - Storing decimal numbers
    
    **Challenges:**  
    - Having to make new constants each time
    
    **Solution:**  
    - Using compound assignment operators to write cleaner code
    
    ---
    
    #**Highlights & Wins**  
    ✅ Gained a deeper understanding of how Swift makes it very easy to write code that is clear and understandable, while at the same time making it hard to accidentally write unsafe code.
    ✅ Strings can be powerful if used correctly in Swift.

---
+++ day01.playground/Contents.swift
@@ -0,0 +1,105 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+//Constants and Variables
+
+var name = "John"
+name = "Jane"
+name = "Randy"
+
+let character = "Ray"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Danny"
+print(playerName)
+
+playerName = "John"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Dom"
+let meaningOfLife = "How many roads must a man walk around?"
+
+// String functionalities
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let quote = "Then he tapped a sign saying \"Believe\and walked away."
+
+//Multi-line String
+
+let movie = """
+A day in
+the life of
+an Apple engineer
+"""
+
+// """ = Line breaks
+
+// Find the character count of a string
+print(actor.count)
+
+// Upper Cased
+print(result.uppercased())
+
+// String pre-fix
+
+print(.movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+// Storing Whole Numbers
+
+let score = 10
+let reallyBig = 100_000_000
+let reallyBig = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+// Special operations that adjust an integer
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(counter)
+
+// Compound assignment operators
+
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+// Storing Decimal Numbers
+
+let a = 1
+let b = 2.0
+let c = a + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name = "Nicolas Cage"
+name = "John Travolta"
+
+var name = "Nicolas Cage"
+name = 57
+
+var rating = 5.0
+rating *= 2
+
+
+
+
